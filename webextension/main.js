c8697d2708730ca1901e0fffc52aec8dbca6ab47
Dirk-jan <dirkjan@sanoweb.nl>
2017-10-29 17:13:08 +0100

    Webext release beta 1

--- webextension/main.js
+++ webextension/main.js
@@ -192,14 +192,3 @@ function handleComplete(resp) {
 
 // Main
 createMenus();
-
-//Future - Check if config sync is needed - we only do it the first time to migrate the configuration
-// let setting = browser.storage.local.get('servers');
-// setting.then(function(items){
-//   if(typeof items['servers'] == 'undefined'){
-//     getConfig();
-//   }
-// });
-
-
-
